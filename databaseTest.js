6fb57a6feb16b032b8e9f306b16d5327795d553a
Patrik Souza <32416501+Patrikol4@users.noreply.github.com>
2020-10-15 14:30:20 -0300

    Teste basico de escrita e leitura no database
    Teste basico e primario para testar a conectividade e a escrita entre o script (app) e o banco de dados (realtime database).

---
+++ databaseTest.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {View, Text, SafeAreaView, TextInput, View, Image} from 'react-native';
+import firebase from '@firebase/app';
+import auth from '@firebase/auth';
+import database from '@firebase/database';
+
+
+export class databaseTest extends React.Component
+
+writeUserData(email,fname,lname){
+  firebase.database().ref('UsersList/').push({
+    email,
+    name,
+  }).then((data)=>{
+    console.tron.log('data ' , data)
+  }).catch((error)=>{
+    console.tron.log('error ' , error)
+  })
+}
+
+// Lendo do Bando de Dados
+readUserData() {
+  firebase
+    .database()
+    .ref('Users/')
+    .on('value', function (snapshot) {
+      console.tron.log(snapshot.val())
+    });
+}
+
