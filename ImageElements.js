6ab917b62efbdaeb64d21e7ec38a9e8c4a1bbaf5
Patrik Souza <32416501+Patrikol4@users.noreply.github.com>
2020-10-31 12:25:41 -0300

    Teste com elemento Image do React Native
    Neste arquivo, inseri um tamanho pré determinado para a imagem e adicionei a ela um blur (borrado).
    E, como podem perceber, chamei a imagem via URI, ou seja, um link externo localizado na internet.

---
+++ ImageElements.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+
+export default function App() {
+
+  const handlePress = () => console.log('Voce pressionou');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Boa-tarde</Text>
+      <Image 
+        blurRadius={2}
+        source={{
+        width: 200,
+        height: 300,        
+        uri: "https://picsum.photos/200/300"}}/>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
